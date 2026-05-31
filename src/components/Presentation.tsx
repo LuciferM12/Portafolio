a7c6c0759efa94fdcd60e0a33c69b601bfb6a605
LuciferM12 <rojasrodriguezmartinomar@gmail.com>
2026-05-30 20:19:25 -0600

    fix(app): change presentation design

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -2,10 +2,10 @@ import { SpeckledBackground } from "./SpeckedBackground";
 
 const Presentation = () => {
   return (
-    <div className="w-full max-w-[1920px] min-h-20 box-border text-dark dark:text-light p-4 flex items-center justify-center">
+    <div className="w-full min-h-screen overflow-x-hidden box-border text-dark dark:text-light p-4 flex items-center justify-center">
       <div className="dark:bg-secondary bg-light max-lg:w-full w-1/2 p-4 min-h-30 flex flex-col items-center rounded-xl justify-center shadow-lg gap-4 relative overflow-hidden">
         <SpeckledBackground />
-        <h1 className="text-3xl font-bold text-center z-10">
+        <h1 className="text-5xl font-bold text-center z-10">
           Hi, I'm Omar Rodriguez 👋
         </h1>
         <p className="text-center mt-2 text-lg text-gray-600 dark:text-gray-300 z-10">
@@ -17,10 +17,10 @@ const Presentation = () => {
           solutions using modern tools and frameworks.
         </p>
         <div className="flex gap-4 items-center z-10">
-          <button className="px-4 py-2 dark:bg-dark bg-black text-light rounded-lg dark:hover:bg-dark/90 transition-colors hover:cursor-pointer">
+          <button className="px-8 py-2 dark:bg-dark bg-black text-light rounded-lg dark:hover:bg-dark/90 transition-colors hover:cursor-pointer">
             Download CV
           </button>
-          <button className="px-4 py-2 border dark:border-dark border-black bg-white dark:bg-secondary dark:text-light rounded-lg dark:hover:bg-secondary/90 transition-colors hover:cursor-pointer ">
+          <button className="px-8 py-2 border dark:border-dark border-black bg-white dark:bg-secondary dark:text-light rounded-lg dark:hover:bg-secondary/90 transition-colors hover:cursor-pointer ">
             Contact Me
           </button>
         </div>
